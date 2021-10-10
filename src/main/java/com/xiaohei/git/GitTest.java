4ff6e01d1b98617abcaaf2813486f18cf4061340
xiaohei1117 <69445200+xiaohei1117@users.noreply.github.com>
2021-10-10 18:04:20 +0800

    Update GitTest.java

--- src/main/java/com/xiaohei/git/GitTest.java
+++ src/main/java/com/xiaohei/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("最新修改");
+        System.out.println("GitHub修改");
     }
 }
